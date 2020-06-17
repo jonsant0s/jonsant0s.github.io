4c101c2df6aa7db356df3f0e197c28c9c99101c5
Jonathan <jawnscott123@gmail.com>
2020-06-17 17:41:48 -0600

    Updates

---
+++ precache-manifest.b43f944034f0a75f7205295e70cc1c3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be1e6c61faabf0909a2a5615026a02f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98893c8654606f49a38c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dfead429822b8c66a379",
+    "url": "/static/js/2.708f0d96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.708f0d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98893c8654606f49a38c",
+    "url": "/static/js/main.db8b7421.chunk.js"
+  },
+  {
+    "revision": "927bafc0a9b222dbc7c4",
+    "url": "/static/js/runtime-main.c871e8c2.js"
+  }
+]);
